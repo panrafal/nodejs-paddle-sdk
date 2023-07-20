8251ccf5fe950f29380e91ea47d5490c8548b92e
Rafał Lindemann <rl@stamina.pl>
2023-07-20 11:31:47 +0200

    Fix CJS

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ export default {
   input: './src/index.ts',
   output: [
     {
-      file: 'dist/node-paddle-sdk.cjs.js',
+      file: 'dist/node-paddle-sdk.cjs',
       format: 'cjs',
     },
     {
